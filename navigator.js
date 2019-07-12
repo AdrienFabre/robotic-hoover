7725b527fab710d8b506aa93126192490943b5b4
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-07-12 15:19:39 +0100

    User can get the number of dirt cleaned up

--- navigator.js
+++ navigator.js
@@ -1,5 +1,3 @@
-import { directive } from "@babel/types";
-
 let fs = require("fs");
 
 class Navigator {
@@ -10,26 +8,47 @@ class Navigator {
       X: parseInt(this.content[1][0]),
       Y: parseInt(this.content[1][2])
     };
+    this.dirtPositions = this.content.splice(2, this.content.length - 3);
+    this.positions = [];
   }
-  finalPosition() {
+
+  executeDrivingInstructions() {
     let position = this.initialPosition;
-    this.directions.forEach(function(direction) {
-      switch (direction) {
-        case "N":
-          position.Y += 1;
-          break;
-        case "S":
-          position.Y -= 1;
-          break;
-        case "E":
-          position.X += 1;
-          break;
-        case "W":
-          position.X -= 1;
-          break;
+    for (let i = 0; i < this.directions.length; i++) {
+      this.updatePosition(position, this.directions[i]);
+    }
+  }
+
+  updatePosition(position, direction) {
+    switch (direction) {
+      case "N":
+        position.Y += 1;
+        break;
+      case "S":
+        position.Y -= 1;
+        break;
+      case "E":
+        position.X += 1;
+        break;
+      case "W":
+        position.X -= 1;
+        break;
+    }
+    this.positions.push(`${position.X} ${position.Y}`);
+  }
+
+  finalPosition() {
+    return this.positions[this.positions.length - 1];
+  }
+
+  cleanedDirt() {
+    let dirtCount = 0;
+    for (let i = 0; i < this.dirtPositions.length; i++) {
+      if (this.positions.includes(this.dirtPositions[i])) {
+        dirtCount += 1;
       }
-    });
-    return `${position.X} ${position.Y}`;
+    }
+    return dirtCount.toString();
   }
 }
 
--- navigator.test.js
+++ navigator.test.js
@@ -5,8 +5,12 @@ describe("Navigator", () => {
 
   beforeEach(() => {
     navigator = new Navigator("input.txt");
+    navigator.executeDrivingInstructions();
   });
   it("Can return the hoover final position", () => {
     expect(navigator.finalPosition()).toBe("1 3");
+  });
+  it("Can return the number of dirt cleaned up", () => {
+    expect(navigator.cleanedDirt()).toBe("1");
   });
 });
